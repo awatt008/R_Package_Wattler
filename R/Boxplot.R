e8669042ca2eb6cf92971d1e40753df81fe06781
aaron <james.wattler@selu.edu>
2021-11-12 14:49:14 +0000

    I added expected inputs and outputs for each function

---
+++ R/Boxplot.R
@@ -0,0 +1,2 @@
+#I will plot species with weight. I expect an output showing median weight associated
+with species with boxes showing max and mins of weight, with whiskers showing outliers.
---
+++ R/LinearRegression.R
@@ -0,0 +1,2 @@
+#I want to look at the weight with the species as a function. I expect my output to be weight as predictor 
+of species with a line of linear regression.
