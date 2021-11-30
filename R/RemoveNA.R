128d9d6a606ddcb0197bfc2d4e13ca269b6a9727
aaron <james.wattler@selu.edu>
2021-11-30 21:19:59 +0000

    Run removeNA when na exists

--- R/RemoveNA.R
+++ R/RemoveNA.R
@@ -3,19 +3,12 @@
 #'
 #'@param . is a piped surveys.csv file
 #'@param weight is the weight column of surveys.csv
-#'@return Returns a surveys.csv without NA values in weight column
+#'@return Returns a surveys.csv without NA values in weight column 
 #'
 no_na_weight <- function(., weight){
-  if(!is.na(surveys$weight)){
-    return("There are no NA values")
-  }
+  if(any(!is.na(surveys$weight)))
   scrub_weight_na <- surveys%>%
   filter(!is.na(weight))
 return(scrub_weight_na)
 }
-#'@examples 
-#'surveys%>%
-#'no_na_weight(weight = weight)
-#
-#is.na(surveys$weight)
 #'
