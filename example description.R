f75cae4981ac1347cb96e804db5fcc86c15ff1e1
aaron <james.wattler@selu.edu>
2021-12-08 02:46:04 +0000

    Updating all

---
+++ example description.R
@@ -0,0 +1,15 @@
+#'multipy together two numbers
+#'
+#'@param x A number
+#'@param y Another number
+#'@param product The product of multiplying x and y
+#'@usage 
+#'mult(4,5)
+
+
+
+
+mult <- function(x,y){
+  product <- x*y
+  return(product)
+}
